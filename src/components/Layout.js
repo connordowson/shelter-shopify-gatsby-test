172a7053868250d4b54e9523c6b732e9dd834aa7
Connor Dowson <connormwdowson@gmail.com>
2021-11-19 10:43:17 +0000

    upgrade gatsby and fix build

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "./navbar";
 import SearchProvider from "../context/search-provider";
+import GlobalStyles from "../styles/global";
+import theme from "../styles/theme";
 
 const Layout = ({ children }) => {
   return (
     <SearchProvider>
       <Navbar />
       <main>{children}</main>
+      <GlobalStyles />
     </SearchProvider>
   );
 };
