dae5b640c42db4d2b80dc4b6993179fb71a61467
Connor Dowson <connormwdowson@gmail.com>
2021-11-19 11:04:58 +0000

    Fix layout import

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import { StoreProvider } from "./src/context/store-context";
 import "normalize.css";
-import Layout from "./src/components/layout";
+import Layout from "./src/components/Layout";
 import theme from "./src/styles/theme";
 
 export const wrapRootElement = ({ element }) => (
