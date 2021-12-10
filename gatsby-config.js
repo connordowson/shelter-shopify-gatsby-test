57d66ffbad95e889b97fddb602f5f301cf3cc189
Connor Dowson <connormwdowson@gmail.com>
2021-12-10 16:56:55 +0000

    update plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,13 +30,6 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
+    "gatsby-source-filesystem",
   ],
 };
